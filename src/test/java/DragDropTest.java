0a836781043880c268b21cddb032bfbb6cd453b7
Stas Yorohov <stas_yorohov@mail.ru>
2024-09-17 00:18:30 +0300

    fix DragDropTest

--- src/test/java/DragDropTest.java
+++ src/test/java/DragDropTest.java
@@ -13,9 +13,20 @@ public class DragDropTest {
     }
 
     @Test
-    void moveDragAndDropTest() {
+    void moveDragAndDropWithActionTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-//        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+    }
+
+    @Test
+    void moveDragAndDropWithoutActionTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDrop(to($("#column-b")));
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
