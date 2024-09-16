ea6509e7cc2c91b2b02d600906dc8aba1dc03719
Stas Yorohov <stas_yorohov@mail.ru>
2024-09-16 17:18:53 +0300

    add action for DragAndDrop

--- src/test/java/DragDropTest.java
+++ src/test/java/DragDropTest.java
@@ -1,9 +1,9 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 public class DragDropTest {
     @BeforeAll
@@ -15,7 +15,8 @@ public class DragDropTest {
     @Test
     void moveDragAndDropTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+//        $("#column-a").dragAndDrop(to($("#column-b")));
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
     }
