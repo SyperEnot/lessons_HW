49a7b569c9e4a3dca67ad4fcb8937ae7373ea077
Stas Yorohov <stas_yorohov@mail.ru>
2024-09-16 16:54:39 +0300

    add DragAndDropTest

---
+++ src/test/java/DragDropTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+public class DragDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void moveDragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+    }
+}
